96e00e8ed93c541e5ceaa69d10f037188dbb30b2
Tomek Buslowski <tbuslowski@gmail.com>
2017-09-11 22:21:38 +0200

    added: MessageCell class and Formatter class

---
+++ outlook-api-tutorial/Formatter.swift
@@ -0,0 +1,34 @@
+//
+//  Formatter.swift
+//  outlook-api-tutorial
+//
+//  Created by Tomek Buslowski on 11.09.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Formatter {
+    class func dateToString(date: JSON) -> String {
+        let graphDateString = date.stringValue
+        if (graphDateString.isEmpty) {
+            return ""
+        }
+        
+        let toDateFormatter = DateFormatter()
+        toDateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        
+        let dateObj = toDateFormatter.date(from: graphDateString)
+        if (dateObj == nil) {
+            return ""
+        }
+        
+        let toStringFormatter = DateFormatter()
+        toStringFormatter.dateStyle = DateFormatter.Style.medium
+        toStringFormatter.timeStyle = DateFormatter.Style.short
+        toStringFormatter.timeZone = TimeZone.current
+        
+        return toStringFormatter.string(from: dateObj!)
+    }
+}
---
+++ outlook-api-tutorial/MessageCell.swift
@@ -0,0 +1,79 @@
+//
+//  MessageCell.swift
+//  outlook-api-tutorial
+//
+//  Created by Tomek Buslowski on 11.09.2017.
+//  Copyright © 2017 Tomek Buslowski. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftyJSON
+
+struct Message {
+    let from: String
+    let received: String
+    let subject: String
+}
+
+class MessageCell: UITableViewCell {
+    
+    @IBOutlet weak var fromLabel: UILabel!
+    @IBOutlet weak var receivedLabel: UILabel!
+    @IBOutlet weak var subjectLabel: UILabel!
+    
+    var from: String? {
+        didSet {
+            fromLabel.text = from
+        }
+    }
+    
+    var received: String? {
+        didSet {
+            receivedLabel.text = received
+        }
+    }
+    
+    var subject: String? {
+        didSet {
+            subjectLabel.text = subject
+        }
+    }
+}
+
+class MessagesDataSource: NSObject {
+    let messages: [Message]
+    
+    init(messages: [JSON]?) {
+        var msgArray = [Message]()
+        
+        if let unwrappedMessages = messages {
+            for (message) in unwrappedMessages {
+                let newMsg = Message(
+                    from: message["from"]["emailAddress"]["name"].stringValue,
+                    received: Formatter.dateToString(date: message["receivedDateTime"]),
+                    subject: message["subject"].stringValue)
+                
+                msgArray.append(newMsg)
+            }
+        }
+        
+        self.messages = msgArray
+    }
+}
+
+extension MessagesDataSource: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: MessageCell.self)) as! MessageCell
+        let message = messages[indexPath.row]
+        cell.from = message.from
+        cell.received = message.received
+        cell.subject = message.subject
+        return cell
+    }
+}
